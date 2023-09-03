c9926ea0de10ccc635be3051e95f475d92c1ce58
fandi-ahmad <fandi4160@gmail.com>
2023-09-03 18:03:15 +0800

    remove block in CORS

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ const cookieParser = require('cookie-parser')
 const { urlencoded } = require('body-parser')
 const { config } = require('dotenv')
 const cors = require('cors')
-
-config()
 const router = require('./routes/router')
+const app = express()
 const port = 8000
 
-const app = express()
 const secretKey = () => { return process.env.SECRET_KEY_COOKIES }
 
 const allowedOrigins = [ process.env.IP_PORT_ACCESS_FRONTEND_1, process.env.IP_PORT_ACCESS_FRONTEND_2, ];
@@ -23,15 +21,16 @@ const corsOptions = {
   },
 };
 
-
+app.use(cors())
+config()
 app.use(urlencoded({ extended: true }))
-app.use(express.json())
 app.use(cookieParser(secretKey()));
-app.use(router)
 app.use(express.static('views'));
 app.set('view engine', 'ejs')
 app.set('views', 'views')
-app.use(cors(corsOptions))
+
+app.use(express.json())
+app.use(router)
 
 
 app.listen(port, () => {
